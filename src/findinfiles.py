98748f2f2fadd07fa9f93a8617783dbabcc46c7d
Roman Piták <roman@pitak.net>
2016-07-29 23:35:59 +0200

    obsolete test removed

--- src/findinfiles.py
+++ src/findinfiles.py
@@ -20,24 +20,22 @@ def files(path):
 def find_in_file(file_name, needle):
 	with open(file_name, 'r') as file_handle:
 		file_mmap = mmap.mmap(file_handle.fileno(), 0, prot=mmap.PROT_READ)
-		results = [x for x in re.finditer(needle, file_mmap)]
-		if results:
-			start, lineno = 0, 1
-			for match in results:
-				while start < match.start():
-					position = file_mmap.find(b'\n', start, match.start())
-					if -1 == position:
-						break
-					lineno += 1
-					start = position + 1
-				file_mmap.seek(start)
-				lines = []
-				for __ in match.group(0).decode().split('\n'):
-					lines.append((lineno, file_mmap.readline().decode()[:-1]))
-					lineno += 1
-				yield lines
-				lineno -= 1
-				start = match.end()
+		start, lineno = 0, 1
+		for match in re.finditer(needle, file_mmap):
+			while start < match.start():
+				position = file_mmap.find(b'\n', start, match.start())
+				if -1 == position:
+					break
+				lineno += 1
+				start = position + 1
+			file_mmap.seek(start)
+			lines = []
+			for __ in match.group(0).decode().split('\n'):
+				lines.append((lineno, file_mmap.readline().decode()[:-1]))
+				lineno += 1
+			yield lines
+			lineno -= 1
+			start = match.end()
 
 
 def main(needle):
