450525b4cb62886134b55b04901faf63340ec8f9
Roman Piták <roman@pitak.net>
2016-07-29 23:29:35 +0200

    prettyfy
    - split into functions
    - remove globals

--- src/findinfiles.py
+++ src/findinfiles.py
@@ -9,33 +9,53 @@ except ImportError:
 	def colored(text, color=None, on_color=None, attrs=None):
 		return text
 
-rrr = re.compile(sys.argv[1].encode(), re.MULTILINE)
 
-with subprocess.Popen(['less', '--no-init', '--quit-if-one-screen'], stdin=subprocess.PIPE) as pager:
-
-	def echo(message=''):
-		pager.stdin.write((str(message) + '\n').encode())
-
-	for file_name in subprocess.check_output(['find', './', '-type', 'f']).decode().split('\n')[:-1]:
+def files(path):
+	for file_name in subprocess.check_output(['find', path, '-type', 'f']).decode().split('\n')[:-1]:
 		if file_name.startswith('./.git'):
 			continue
-		with open(file_name, 'r') as file_handle:
-			data = mmap.mmap(file_handle.fileno(), 0, prot=mmap.PROT_READ)
-			res = [x for x in re.finditer(rrr, data)]
-			if res:
-				echo(colored(file_name, 'red', attrs=['bold']))
-				start, lineno = 0, 1
-				for x in res:
-					while start < x.start():
-						position = data.find(b'\n', start, x.start())
-						if -1 == position:
-							break
-						lineno += 1
-						start = position + 1
-					data.seek(start)
-					for line in x.group(0).decode().split('\n'):
-						echo(colored(str(lineno) + ':', 'green') + data.readline().decode()[:-1])
-						lineno += 1
-					lineno -= 1
-					start = x.end()
-					echo()
+		yield file_name
+
+
+def find_in_file(file_name, needle):
+	with open(file_name, 'r') as file_handle:
+		file_mmap = mmap.mmap(file_handle.fileno(), 0, prot=mmap.PROT_READ)
+		results = [x for x in re.finditer(needle, file_mmap)]
+		if results:
+			start, lineno = 0, 1
+			for match in results:
+				while start < match.start():
+					position = file_mmap.find(b'\n', start, match.start())
+					if -1 == position:
+						break
+					lineno += 1
+					start = position + 1
+				file_mmap.seek(start)
+				lines = []
+				for __ in match.group(0).decode().split('\n'):
+					lines.append((lineno, file_mmap.readline().decode()[:-1]))
+					lineno += 1
+				yield lines
+				lineno -= 1
+				start = match.end()
+
+
+def main(needle):
+	with subprocess.Popen(['less', '--no-init', '--quit-if-one-screen'], stdin=subprocess.PIPE) as pager:
+
+		def echo(message=''):
+			pager.stdin.write((str(message) + '\n').encode())
+
+		for file_name in files('./'):
+			first_result = True
+			for result in find_in_file(file_name, needle):
+				if first_result:
+					echo(colored(file_name, 'red', attrs=['bold']))
+					first_result = False
+				for line_number, line in result:
+					echo(colored(str(line_number) + ':', 'green') + line)
+				echo()
+
+
+if '__main__' == __name__:
+	main(re.compile(sys.argv[1].encode(), re.MULTILINE))
